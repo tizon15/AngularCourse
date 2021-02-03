4600bee466a99eaa4a57be873aeaac884dbd64b6
Alvaro Martínez-Tizón <tizon15@gmail.com>
2021-02-03 16:45:54 +0100

    End of components

--- firstProject/src/app/servers/servers.component.ts
+++ firstProject/src/app/servers/servers.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /*
+  * The '[]' makes the selector as an attribute
+  * selector: '[app-servers]',
+  */
+  /* 
+  * Put a dot before makes the selector as a class
+  * selector: '.app-servers',
+   */
   selector: 'app-servers',
   template: `
    <app-server></app-server>
