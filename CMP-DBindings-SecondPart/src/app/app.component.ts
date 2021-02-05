83e292165cae113c3c5ce63ec1a9bb7ff786825f
Alvaro Martínez-Tizón <tizon15@gmail.com>
2021-02-05 13:14:23 +0100

    @Input() Binding custom directive

--- CMP-DBindings-SecondPart/src/app/app.component.ts
+++ CMP-DBindings-SecondPart/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CMP-DBindings-SecondPart';
+  serverElements = [
+    { type: 'server', name: 'TestServer', content: 'Just a test' },
+  ];
 }
--- CMP-DBindings-SecondPart/src/app/app.module.ts
+++ CMP-DBindings-SecondPart/src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {PanelModule} from 'primeng/panel';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ServerElementComponent, CockpitComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    PanelModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
